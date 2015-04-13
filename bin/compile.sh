c9630b8f603bdf31a487758b4c4e9146b0fc957d
kartik <kartik@cs.umd.edu>
2015-04-13 00:29:02 -0400

    Fix links

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,4 @@
 find ../ -name "*.java" > source.txt;
-#find ../../my_FlexSC/ -name "*.java" >> source.txt;
-javac -cp .:../lib/*:../lib/FlexSC.jar -d . @source.txt;
+#javac -cp .:../lib/FlexSC.jar:../lib/commons-cli-1.2.jar:../lib/commons-io-2.4.jar -d . @source.txt;
+javac -cp .:../lib/* -d . @source.txt;
 rm source.txt;
